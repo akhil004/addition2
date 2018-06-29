fcf75731fac9e731a467a49ff63001fcd0b7e7c7
Raj <rajthakur.1199@gmail.com>
2018-06-29 18:25:43 +0530

    Added a function to add numbers in the array.

--- index.js
+++ index.js
@@ -4,3 +4,14 @@ module.exports.add = (a,b) => {
 module.exports.addThreeDigit = (a,b,c) => {
     return a + b + c;
 }
+
+
+/*** Adsd numbersin an array ***/
+
+module.exports.addArray = (c) => {
+    sum = 0;
+    c.forEach(function(value, index){
+        sum += value;
+    });
+    return sum;
+}
