6144b7bffedd80fd455436a07cfad6ad279761b4
Pyshko <44314385+Pyshko@users.noreply.github.com>
2018-11-21 11:44:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-module.exports.add = (a,b) => {
-    return a + b;
-}
-module.exports.addThreeDigit = (a,b,c) => {
-    return a + b + c;
-}
-
-
-/*** Adsd numbersin an array ***/
-
-module.exports.addArray = (c) => {
-    sum = 0;
-    c.forEach(function(value, index){
-        sum += value;
-    });
-    return sum;
-}
+module.exports.add = (...numbers) => numbers.reduce((acc, n) => acc + n, 0);
